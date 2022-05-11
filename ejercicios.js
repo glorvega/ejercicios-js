43194ba84faf98cb74f816c1ac0e8c8eaec024f2
glorvega <gloria.vega@bootcamp-upgrade.com>
2022-05-11 20:25:16 +0200

    subida ejercicios

---
+++ ejercicios.js
@@ -0,0 +1,52 @@
+/*EJERCICIO 1*/
+
+/* 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+var myFavoriteHero = 'Hulk';
+
+1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+var x = 50;
+
+1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+var h = 5;
+var y = 10;
+
+1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+var z = (h + y);
+
+
+//*EJERCICIO 2*//* / */
+
+/* 1.1 
+Dado name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000}; */
+
+/*EJERCICIO 3*/
+
+/* 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert. */ 
+var multiplicacion = (10 * 5);
+alert(multiplicacion);
+
+1.2 Divide 10 por 2 y muestra el resultado en un alert.
+var division = (10 / 2);
+alert(division);
+
+
+1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+var operacion = (15 % 9);
+alert(operacion);
+
+
+1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5.
+var y = 10;
+var z = 5;
+var x = (y + z);
+console.log(x);
+
+
+1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5.
+var y = 10;
+var z = 5;
+var x = (y * z);
+console.log(x);
